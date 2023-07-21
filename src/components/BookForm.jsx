c5e3a3bfdbe46c724b4661401e164e80ae541718
Pavel Belousov <signorpao@gmail.com>
2023-07-21 12:34:38 +0200

    form.

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+import CheckIn from "./CheckIn";
+import CheckOut from "./CheckOut";
+import AdultsDropdown from "./AdultsDropdown";
+import KidsDropdown from "./KidsDropdown";
+
+import { RoomContext } from "../context/RoomContext";
 
 const BookForm = () => {
-  return (
-    <div>BookForm</div>
-  )
-}
+  const { handleClick } = useContext(RoomContext);
 
-export default BookForm
+  return (
+    <form className="h-[300px] w-full lg:h-[70px]">
+      <div className="flex flex-col h-full w-full lg:flex-row">
+        <div className="flex-1 border-r">
+          <CheckIn />
+        </div>
+        <div className="flex-1 border-r">
+          <CheckOut />
+        </div>
+        <div className="flex-1 border-r">
+          <AdultsDropdown />
+        </div>
+        <div className="flex-1 border-r">
+          <KidsDropdown />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={(e) => handleClick(e)}
+        >
+          Check now
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default BookForm;
--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 import { roomData } from "../data";
 
@@ -6,9 +6,29 @@ export const RoomContext = createContext();
 
 const RoomProvider = ({ children }) => {
   const [rooms, setRooms] = useState(roomData);
+  const [adults, setAdults] = useState("1 Adult");
+  const [kids, setKids] = useState("0 Kids");
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(Number(adults[0]) + Number(kids[0]));
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const newRooms = roomData.filter((room) => {
+      return total <= room.maxPerson;
+    });
+    setRooms(newRooms);
+  };
 
   return (
-    <RoomContext.Provider value={{ rooms }}>{children}</RoomContext.Provider>
+    <RoomContext.Provider
+      value={{ rooms, adults, setAdults, kids, setKids, handleClick }}
+    >
+      {children}
+    </RoomContext.Provider>
   );
 };
 
